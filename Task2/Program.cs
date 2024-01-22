e4492bc3ea652f7cde4c72f52d19fe1d4df1538c
IgorK <gosha424@mail.ru>
2024-01-22 12:20:41 +0300

    Task1

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве. */
+ void PrintData(string res)
+ {
+     Console.WriteLine(res);
+ }
+
+ int[] Gen1DArray(int len, int minValue, int maxValue)
+ {
+     int[] arr = new int[len];
+     for (int i = 0; i < arr.Length; i++)
+     {
+         arr[i] = new Random().Next(minValue, maxValue + 1);
+     }
+     return arr;
+ }
+ 
+
+void Print1DArr(int[] arr)
+{
+     Console.Write("[");
+     for (int i = 0; i < arr.Length - 1; i++)
+     {
+         Console.Write(arr[i] + ", ");
+     }
+     Console.WriteLine(arr[arr.Length - 1] + "]");
+ }
+
+
+ int CountElem(int[] arr)
+ {
+     int res = 0;
+     for (int i = 0; i < arr.Length; i++)
+     {
+         if (Test(arr[i]))
+         {
+             res++;
+         }
+     }
+     return res;
+ }
+
+ bool Test(int n)
+ {
+     return (n % 2 == 0);
+ }
+
+
+
+int[] testArr = Gen1DArray(10,100,999);
+
+Print1DArr(testArr);
+
+int count = CountElem(testArr);
+
+PrintData("Количество четных элементов равно: "+count);
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)*/
+
+int[] arr = GenArray(20,1,100);
+int[] copyArr = SwapNewArray(arr);
+Console.WriteLine("Исходны массив");
+Print1DArr(arr); 
+Console.WriteLine("Новый массив перевернутый");
+Print1DArr(copyArr);
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int[] SwapNewArray(int[] arr)
+{
+    int[] outArr = new int[arr.Length];
+    for(int i =0;i<arr.Length;i++)
+    {
+        outArr[i] = arr[arr.Length-1-i];
+    }
+    return outArr;
+}
